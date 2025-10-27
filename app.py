827fa85988ae217082b922fd7ec004699b1301da
Vijayvdm <115135453+Vijayvdm@users.noreply.github.com>
2025-10-27 16:11:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ XRayMiddleware(app, xray_recorder)
 
 @app.route('/')
 def index():
-    return "Hello from ECS with AWS X-Ray!"
+    return "Hi Lingesh Hope you are doing good!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
